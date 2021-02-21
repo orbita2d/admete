deb4aabd9acac083fe382ea2dac182c05207a6f4
orbita2d <orbita@orbital.me.uk>
2021-02-21 16:35:25 +0000

    Changed the piece numberings so they cover less number space.

--- src/game/piece.hpp
+++ src/game/piece.hpp
@@ -3,21 +3,22 @@
 
 #pragma once
 
+#define CMASK 0x18
+#define PMASK 0x07
+
 class Piece
 {
 public:
 
     Piece() = default;
     constexpr Piece(int aPiece) : value(uint8_t(aPiece)) { };
-    constexpr Piece(bool is_white) : value(is_white ? 0x20 : 0x10) {};
+    constexpr Piece(bool is_white) : value(is_white ? 0x10 : 0x08) {};
 
     constexpr bool operator==(Piece that) const { return value == that.value; }
     constexpr bool operator!=(Piece that) const { return value != that.value; }
     constexpr Piece operator~() const {
         // Same piece but opposite colour
-        const uint8_t colour_mask = 0x30;
-        const uint8_t piece_mask = 0x0F;
-        return ((~value) & colour_mask) | (value & piece_mask);
+        return ((~value) & CMASK) | (value & PMASK);
 
     }
 
@@ -26,30 +27,29 @@ public:
     char pretty_print() const;
 
     constexpr Piece get_piece() const{
-        const uint8_t mask = 0x0F;
-        return Piece(value & mask);
+        return Piece(value & PMASK);
     }
     constexpr Piece get_colour() const{
-        const uint8_t mask = 0x30;
+        const uint8_t mask = CMASK;
         return Piece(value & mask);
     }
 
     constexpr bool is_colour(Piece colour) const {
-        const uint8_t mask = 0x30;
+        const uint8_t mask = CMASK;
         return uint8_t(get_colour()) == (colour.value & mask);
     }
 
     constexpr bool is_piece(Piece piece) const {
-        const uint8_t mask = 0x0F;
+        const uint8_t mask = PMASK;
         return uint8_t(get_piece()) == (piece.value & mask);
     }
 
     constexpr bool is_white() const {
-        return (value & 0x30) == 0x10;
+        return (value & CMASK) == 0x08;
     }
 
     constexpr bool is_black() const {
-        return (value & 0x30) == 0x20;
+        return (value & CMASK) == 0x10;
     }
 
     constexpr bool is_blank() const {
@@ -57,27 +57,27 @@ public:
     }
 
     constexpr bool is_pawn() const {
-        return (value & 0x0F) == 0x01;
+        return (value & PMASK) == 0x01;
     }
 
     constexpr bool is_knight() const {
-        return (value & 0x0F) == 0x02;
+        return (value & PMASK) == 0x02;
     }
 
     constexpr bool is_bishop() const {
-        return (value & 0x0F) == 0x03;
+        return (value & PMASK) == 0x03;
     }
 
     constexpr bool is_rook() const {
-        return (value & 0x0F) == 0x04;
+        return (value & PMASK) == 0x04;
     }
 
     constexpr bool is_queen() const {
-        return (value & 0x0F) == 0x05;
+        return (value & PMASK) == 0x05;
     }
 
     constexpr bool is_king() const {
-        return (value & 0x0F) == 0x06;
+        return (value & PMASK) == 0x06;
     }
 
     constexpr uint8_t get_value() const {return value; }
@@ -97,8 +97,8 @@ namespace Pieces {
     static constexpr Piece Rook = Piece(4);
     static constexpr Piece Queen = Piece(5);
     static constexpr Piece King = Piece(6);
-    static constexpr Piece White = Piece(16);
-    static constexpr Piece Black = Piece(32);
+    static constexpr Piece White = Piece(8);
+    static constexpr Piece Black = Piece(16);
 }
 
 std::ostream& operator<<(std::ostream& os, const Piece piece);
