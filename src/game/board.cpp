ca98cf1de0159adc7d32ef35fb6c39758ea7f9fc
orbita2d <orbita@orbital.me.uk>
2021-02-23 23:56:06 +0000

    Organised code a little

--- src/game/board.cpp
+++ src/game/board.cpp
@@ -1274,60 +1274,6 @@ bool Board::is_in_check() const {
     return is_check(king_square, colour);
 }
 
-bool in_line(const Square origin, const Square target){
-    if (origin.file() == target.file()) {return true; }
-    if (origin.rank() == target.rank()) {return true; }
-    if (origin.anti_diagonal() == target.anti_diagonal()) {return true; }
-    if (origin.diagonal() == target.diagonal()) {return true; }
-    return false;
-}
-
-bool in_line(const Square p1, const Square p2, const Square p3){
-    if (p1.file() == p2.file() & p1.file() == p3.file()) {return true; }
-    if (p1.rank() == p2.rank() & p1.rank() == p3.rank()) {return true; }
-    if (p1.diagonal() == p2.diagonal() & p1.diagonal() == p3.diagonal()) {return true; }
-    if (p1.anti_diagonal() == p2.anti_diagonal() & p1.anti_diagonal() == p3.anti_diagonal()) {return true; }
-    return false;
-}
-
-bool interposes(const Square origin, const Square target, const Square query) {
-    if (origin.file() == target.file()) {
-        if (origin.file() != query.file()) {return false;}
-        if ((query.rank() > std::min(origin.rank(), target.rank())) & (query.rank() < std::max(origin.rank(), target.rank()))) {
-            return true;
-        } else { return false;}
-    }
-    if (origin.rank() == target.rank()) {
-        if (origin.rank() != query.rank()) {return false;}
-        if ((query.file() > std::min(origin.file(), target.file())) & (query.file() < std::max(origin.file(), target.file()))) {
-            return true;
-        } else { return false;}
-    }
-
-    if (origin.diagonal() == target.diagonal()) {
-        if (origin.diagonal() != query.diagonal()) {return false;}
-        if ((query.anti_diagonal() > std::min(origin.anti_diagonal(), target.anti_diagonal())) & (query.anti_diagonal() < std::max(origin.anti_diagonal(), target.anti_diagonal()))) {
-            return true;
-        } else { return false;}
-    }
-
-    if (origin.anti_diagonal() == target.anti_diagonal()) {
-        if (origin.anti_diagonal() != query.anti_diagonal()) {return false;}
-        if ((query.diagonal() > std::min(origin.diagonal(), target.diagonal())) & (query.diagonal() < std::max(origin.diagonal(), target.diagonal()))) {
-            return true;
-        } else { return false;}
-    }
-
-    return false;
-}
-
-int what_dirx(const Square origin, const Square target){
-    if (origin.file() == target.file()) {return origin.rank_index() < target.rank_index() ? 2 : 0; }
-    if (origin.rank() == target.rank()) {return origin.file_index() < target.file_index() ? 1 : 3; }
-    if (origin.anti_diagonal() == target.anti_diagonal()) {return origin.diagonal() < target.diagonal() ? 5 : 7; }
-    if (origin.diagonal() == target.diagonal()) {return origin.anti_diagonal() < target.anti_diagonal() ? 6 : 4; }
-    return -1;
-}
 
 bool Board::is_pinned(const Square origin) const{
     for (Square target : pinned_pieces) {
