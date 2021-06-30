516f061d54260740112626e4be5784068b895422
Kylie MacFarquharson <kylie.macfarquharson@physics.ox.ac.uk>
2021-06-30 13:22:35 +0100

    Tweaked move ordering

--- src/search/ordering.cpp
+++ src/search/ordering.cpp
@@ -85,8 +85,10 @@ score_t see(Board &board, const Square target, Colour side, PieceType pt, Bitboa
         return 0;
     } else {
         for (depth_t d = depth - 2; d > 0; d--) {
-            // The oponent won't give us material, so the gain at depth d-1 is going to be whichever is worse of if they
-            // capture or don't.
+            // The oponent won't give us material, so the gain at depth d-1 is going to be whichever is worse (for us)
+            // of if they capture or don't.
+            // Their gain at depth d, is (gain[d]), our gain if they do that is -gain[d]. (That is, if they capture)
+            // If they don't capture, our gain is gain[d-1], because they do something else.
             gain[d - 1] = std::min(gain[d - 1], (score_t)-gain[d]);
         }
 
@@ -99,34 +101,36 @@ score_t see_capture(Board &board, const Move move) {
     assert(move.is_capture());
     assert(move != NULL_MOVE);
     Bitboard mask = Bitboards::omega ^ move.origin;
-    score_t see_gain = see(board, move.target, ~board.who_to_play(), move.moving_piece, mask);
+    const score_t see_gain = see(board, move.target, ~board.who_to_play(), move.moving_piece, mask);
     score_t cap_gain;
     if (move.is_ep_capture()) {
         cap_gain = Evaluation::piece_material(PAWN);
     } else {
         cap_gain = Evaluation::piece_material(board.piece_type(move.target));
     }
-    score_t gain = cap_gain - see_gain;
-    return gain;
+    return cap_gain - see_gain;
 }
 
 score_t see_quiet(Board &board, const Move move) {
     assert(move.is_quiet());
     assert(move != NULL_MOVE);
     Bitboard mask = Bitboards::omega ^ move.origin;
-    score_t see_gain = see(board, move.target, ~board.who_to_play(), move.moving_piece, mask);
-    score_t gain = -see_gain;
-    return gain;
+    const score_t see_gain = see(board, move.target, ~board.who_to_play(), move.moving_piece, mask);
+    return -see_gain;
 }
 
 void sort_moves(Board &board, MoveList &legal_moves, const DenseMove hash_dmove, const KillerTableRow killer_moves) {
-    MoveList checks, quiet_moves, good_captures, bad_captures, sorted_moves, killer;
+    MoveList checks, quiet_moves, good_captures, even_captures, bad_captures, sorted_moves, killer;
     size_t n_moves = legal_moves.size();
-    checks.reserve(n_moves);
-    quiet_moves.reserve(n_moves);
+    sorted_moves.reserve(n_moves);
     good_captures.reserve(n_moves);
+    checks.reserve(n_moves);
+    killer.reserve(n_krow);
+    even_captures.reserve(n_moves);
+    quiet_moves.reserve(n_moves);
+    bad_captures.reserve(n_moves);
     // The moves from get_moves already have captures first
-    for (Move move : legal_moves) {
+    for (Move &move : legal_moves) {
         if (move == hash_dmove) {
             // The search handles the hash move itself. Here we just make sure it doesn't end up in the final move
             // list.
@@ -134,32 +138,41 @@ void sort_moves(Board &board, MoveList &legal_moves, const DenseMove hash_dmove,
             killer.push_back(move);
         } else if (board.gives_check(move)) {
             checks.push_back(move);
-        } else if (move.is_ep_capture()) {
-            // En-passent is weird too.
-            move.captured_piece = PAWN;
-            good_captures.push_back(move);
         } else if (move.is_capture()) {
             // Make sure to lookup and record the piece captured
             move.captured_piece = board.piece_type(move.target);
-            move.score =
-                Evaluation::piece_material(move.captured_piece) - Evaluation::piece_material(move.moving_piece);
-            if (move.score >= 0) {
-                // Captures of a more valuable piece with a less valuable piece are almost always good.
+            move.score = Ordering::see_capture(board, move);
+            if (move.score >= 50) {
                 good_captures.push_back(move);
+            } else if (move.score >= -50) {
+                even_captures.push_back(move);
             } else {
                 bad_captures.push_back(move);
             }
         } else {
+            move.score = Ordering::see_quiet(board, move);
             quiet_moves.push_back(move);
         }
     }
-    legal_moves.clear();
-    legal_moves.insert(legal_moves.end(), checks.begin(), checks.end());
     std::sort(good_captures.begin(), good_captures.end(), cmp);
+    std::sort(even_captures.begin(), even_captures.end(), cmp);
     std::sort(bad_captures.begin(), bad_captures.end(), cmp);
+    std::sort(quiet_moves.begin(), quiet_moves.end(), cmp);
+
+    // Move order is:
+    // Captures with SEE > 50
+    // Checks
+    // Killer moves
+    // Captures -50 < SEE < 50
+    // Quiet moves
+    // Captures with SEE < -50
+
+    legal_moves.clear();
     legal_moves.insert(legal_moves.end(), good_captures.begin(), good_captures.end());
+    legal_moves.insert(legal_moves.end(), checks.begin(), checks.end());
     legal_moves.insert(legal_moves.end(), killer.begin(), killer.end());
-    legal_moves.insert(legal_moves.end(), bad_captures.begin(), bad_captures.end());
+    legal_moves.insert(legal_moves.end(), even_captures.begin(), even_captures.end());
     legal_moves.insert(legal_moves.end(), quiet_moves.begin(), quiet_moves.end());
+    legal_moves.insert(legal_moves.end(), bad_captures.begin(), bad_captures.end());
 }
 } // namespace Ordering
