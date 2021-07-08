f1abdd229fe409e74d9ddccd1d45dd4c9759ac30
Kylie MacFarquharson <kylie.macfarquharson@physics.ox.ac.uk>
2021-07-08 18:27:43 +0100

    Prune moves in qsearch that are unlikely to raise alpha

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -488,10 +488,15 @@ score_t Search::quiesce(Board &board, const score_t alpha_start, const score_t b
     // Sort the captures and record SEE.
     Ordering::rank_and_sort_moves(board, moves, NULL_DMOVE, NULL_KROW);
 
+    constexpr score_t see_margin = 100;
     for (Move move : moves) {
         // For a capture, the recorded score is the SEE value.
         // It makes sense to not consider losing captures in qsearch.
-        if (move.is_capture() && (move.score < 0)) {
+        if (!board.is_check() && move.is_capture() && (move.score < 0)) {
+            continue;
+        }
+        // In qsearch, only consider moves with a decent chance of raising alpha.
+        if (!board.is_check() && move.is_capture() && (stand_pat + move.score < alpha - see_margin)) {
             continue;
         }
         board.make_move(move);
