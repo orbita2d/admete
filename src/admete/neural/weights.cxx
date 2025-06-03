9e065f9db592641fa374c4b30cab55744e10cd2b
Kylie MacFarquharson <orbita@orbital.me.uk>
2025-06-03 08:04:08 +0100

    Retrained with new data loading, ~ +30 elo

--- src/admete/neural/weights.cxx
+++ src/admete/neural/weights.cxx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83aedc77d3a0944ba62cf97fe5d4bf1abf3097d85e85139accd5d6a894359282
-size 1149497
+oid sha256:6ea16abe9d48870defbcbf0fab74cad0727b3399ee215217f96c0a20d5a32805
+size 1148962
